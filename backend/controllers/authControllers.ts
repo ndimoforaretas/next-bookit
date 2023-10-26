cadc4a578b46a07192ff38de470da94326e23eb5
Ndimofor <ndimaret@gmail.com>
2023-10-26 02:42:17 +0200

    add user auth middleware

--- backend/controllers/authControllers.ts
+++ backend/controllers/authControllers.ts
@@ -19,4 +19,18 @@ export const registerUser = catchAsyncErrors(async (req: NextRequest) => {
   });
 });
 
-// Login user   =>   /api/auth/login
+// Update user profile   =>   /api/me/update
+export const updateProfile = catchAsyncErrors(async (req: NextRequest) => {
+  const body = await req.json();
+
+  const userData = {
+    name: body.name,
+    email: body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user._id, userData);
+
+  return NextResponse.json({
+    success: true,
+  });
+});
