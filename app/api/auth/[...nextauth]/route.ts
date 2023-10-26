d03ed0de04f1f814a8de832be3677c84e7ae9846
Ndimofor <ndimaret@gmail.com>
2023-10-26 10:47:04 +0200

    protect user route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -67,6 +67,9 @@ async function auth(req: NextApiRequest, res: NextApiResponse) {
         return session;
       },
     },
+    pages: {
+      signIn: "/login",
+    },
     secret: process.env.NEXTAUTH_SECRET,
   });
 }
